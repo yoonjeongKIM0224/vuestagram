a8f98229fee76eea8492008e39da31c996b237d4
yoonjeong <88982365+yoonjeongKIM0224@users.noreply.github.com>
2024-03-25 22:12:53 +0900

    포스트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="header">
+    <ul class="header-button-left">
+      <li>Cancel</li>
+    </ul>
+    <ul class="header-button-right">
+      <li>Next</li>
+    </ul>
+    <img src="./assets/logo.png" class="logo" />
+  </div>
+
+  <Container :postData="postData" />
+
+  <div class="footer">
+    <ul class="footer-button-plus">
+      <input type="file" id="file" class="inputfile" />
+      <label for="file" class="input-plus">+</label>
+    </ul>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Container from './components/TheContainer.vue';
+import PostData from './assets/postData.js';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      postData: PostData
+    }
+  },
   components: {
-    HelloWorld
+    Container,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('./assets/style.css');
 </style>
---
+++ src/components/TheContainer.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Post 
+        v-for="(item, idx) in postData"
+        :key="idx"
+        :item="item" />
+    </div>
+  </template>
+  
+  <script>
+  import Post from './ThePost.vue';
+  
+  export default {
+    name: 'TheContaienr',
+    props: {
+        postData: Array
+    },
+    components: {
+        Post,
+    }
+  }
+  </script>
+  
