d22a3fdb63eb961628cfa769918148c61b67b29d
kimyoonjeong <gnoejnooy@gmail.com>
2024-03-26 23:42:45 +0900

    더보기 게시글 추가

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,9 @@
   </div>
 
   <Container :postData="postData" />
+  <button type="button" @click="btnNum++, more()">더보기</button>
+
+  {{ btnNum }}
 
   <div class="footer">
     <ul class="footer-button-plus">
@@ -22,17 +25,36 @@
 <script>
 import Container from './components/TheContainer.vue';
 import PostData from './assets/postData.js';
+import axios from 'axios';
 
 export default {
   name: 'App',
   data(){
     return {
-      postData: PostData
+      postData: PostData,
+      btnNum: 0,
     }
   },
   components: {
     Container,
-  }
+  },
+  methods: {
+    more(){
+      let link = '';
+
+      if(this.btnNum === 1){
+        link = 'https://codingapple1.github.io/vue/more0.json';
+      } else if (this.btnNum === 2) {
+        link = 'https://codingapple1.github.io/vue/more1.json';
+      }
+
+      axios.get(link)
+      .then((result) => { //data: GET요청으로 가져온 데이터
+        // 요청 성공 시 실행할 코드
+        this.postData.push(result.data);
+      })
+    }
+  },
 }
 </script>
 
